1f21d12e0e2af2599b66ce2fcc47e661daa298c1
Sennett Lau <laub1199@gmail.com>
2024-03-11 19:10:55 +0800

    fmt

--- src/examples/ERC7629Example.sol
+++ src/examples/ERC7629Example.sol
@@ -3,15 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import  "../ERC7629.sol";
+import "../ERC7629.sol";
 
 contract ERC7629Example is Ownable, ERC7629 {
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
-        uint256 units_
-    ) ERC7629(name_, symbol_, decimals_, units_) Ownable(msg.sender) {
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 units_)
+        ERC7629(name_, symbol_, decimals_, units_)
+        Ownable(msg.sender)
+    {
         _mintERC20(msg.sender, 10_000 * units_ * 10 ** decimals_);
     }
 
--- test/mock/ERC7629Mock.sol
+++ test/mock/ERC7629Mock.sol
@@ -6,12 +6,10 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "../../src/ERC7629.sol";
 
 contract ERC7629Mock is Ownable, ERC7629 {
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
-        uint256 units_
-    ) ERC7629(name_, symbol_, decimals_, units_) Ownable(msg.sender) {}
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 units_)
+        ERC7629(name_, symbol_, decimals_, units_)
+        Ownable(msg.sender)
+    {}
 
     function mintERC20(address to_, uint256 amount_) public {
         _mintERC20(to_, amount_);
@@ -21,10 +19,7 @@ contract ERC7629Mock is Ownable, ERC7629 {
         _mintERC721(to_, id_);
     }
 
-    function tokenURI(
-        uint256 id_
-    ) public pure override returns (string memory) {
-        return
-            string.concat("https://example.com/token/", Strings.toString(id_));
+    function tokenURI(uint256 id_) public pure override returns (string memory) {
+        return string.concat("https://example.com/token/", Strings.toString(id_));
     }
 }
